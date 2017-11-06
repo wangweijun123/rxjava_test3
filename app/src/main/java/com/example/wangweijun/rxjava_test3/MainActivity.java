b5f0b426d6a9a69b1574fb92c146370638e6fc63
wangweijun <262448350@qq.com>
2017-11-06 23:48:14 +0800

    add check short cut

--- app/src/main/java/com/example/wangweijun/rxjava_test3/MainActivity.java
+++ app/src/main/java/com/example/wangweijun/rxjava_test3/MainActivity.java
@@ -332,11 +332,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void getFirstPageAndRankList(View v) {
-        ApiService.getFirstPageAndRankList();
+//        ApiService.getFirstPageAndRankList();
+//        boolean flag = hasShortcut(this, "Rxjava_test3");
+        boolean flag = launcherUtil.isShortCutExist(getApplicationContext(), "北京燃气");
+        Log.i(TAG, "flag: " + flag);
     }
-
-
-
 
     @Override
     protected void onDestroy() {
